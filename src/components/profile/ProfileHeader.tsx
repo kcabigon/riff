64266ab43bd1ee2b218c8428d3b2268c825e4098
jarric22 <jarric22@gmail.com>
2026-04-28 20:53:49 -0700

    fix: remove divider between My stats and Settings in profile dropdown

--- src/components/profile/ProfileHeader.tsx
+++ src/components/profile/ProfileHeader.tsx
@@ -42,7 +42,6 @@ export default function ProfileHeader({
             label: "My stats",
             onClick: () => setIsStatsOpen(true),
           },
-          { type: "divider" as const },
         ]
       : []),
     ...(isOwnProfile
