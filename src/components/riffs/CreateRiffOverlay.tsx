fee557be6d857226a638a741fe81c41945d6c9cd
jarric22 <jarric22@gmail.com>
2026-09-24 11:15:37 -0700

    copy: add "The Creative Brief" heading to slide 1, replace back arrow
    Slide 1 doesn't need a back arrow (back = close, same as X); an h1
    sized to the arrow's row height keeps slide 2's fields positionally
    consistent with slide 1's.

--- src/components/riffs/CreateRiffOverlay.tsx
+++ src/components/riffs/CreateRiffOverlay.tsx
@@ -174,7 +174,31 @@ export default function CreateRiffOverlay({
               textAlign: "left",
             }}
           >
-            {step < 3 && (
+            {step === 1 && (
+              <div
+                style={{
+                  width: "100%",
+                  height: "32px",
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                <h1
+                  style={{
+                    fontFamily: "var(--font-dm-serif-text)",
+                    fontSize: "24px",
+                    fontWeight: 400,
+                    lineHeight: "32px",
+                    color: "#000000",
+                    margin: 0,
+                  }}
+                >
+                  The Creative Brief
+                </h1>
+              </div>
+            )}
+
+            {step === 2 && (
               <div style={{ width: "100%", display: "flex" }}>
                 <BackButton onClick={handleBack} />
               </div>
